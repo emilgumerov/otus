5dca7bf977991942a155732347f1b59664967ab7
emilgumerov <emilgumerov@a.ru>
2025-11-28 16:23:14 +0300

    7 comm

--- testprog.c
+++ testprog.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 int main() {
-
-
-    
     printf("hey you!\n");
-    printf("that is the test Program!\n");
+    printf("that is the test Program! 7 commit\n");
 return 0;
 }
 
